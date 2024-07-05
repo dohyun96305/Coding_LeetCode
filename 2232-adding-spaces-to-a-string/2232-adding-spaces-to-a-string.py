53b36d5d1a04c0b4a8208d891b68d58f02bee15a
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-05 14:56:45 +0900

    Time: 476 ms (93.55%), Space: 49.2 MB (64.52%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def addSpaces(self, s, spaces):
+        """
+        :type s: str
+        :type spaces: List[int]
+        :rtype: str
+        """
+        answer = []
+        temp = 0
+        
+        for idx in spaces : 
+            answer.append(s[temp:idx])
+            temp = idx
+
+        return ' '.join(answer + [s[temp:]])
+
+        
