63e355720316877df59344e9f997dbd7dee029c1
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-06 16:09:06 +0900

    Time: 339 ms (88.94%), Space: 27.2 MB (43.22%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.py
+++ 2634-minimum-common-value/2634-minimum-common-value.py
@@ -5,17 +5,16 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-
         i, j = 0, 0
 
         while i < len(nums1) and j < len(nums2) : 
             if nums1[i] == nums2[j] : 
                 return nums1[i]
+                
+            if nums1[i] > nums2[j] :
+                j += 1
             else : 
-                if nums1[i] > nums2[j] :
-                    j += 1
-                else : 
-                    i += 1
+                i += 1
 
         return -1
 
