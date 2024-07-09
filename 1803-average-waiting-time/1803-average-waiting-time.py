02122ac5f7d1c4dec2cbb3cc469bd34305870a21
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-09 14:46:50 +0900

    Time: 746 ms (17.5%), Space: 56.1 MB (55%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -7,14 +7,14 @@ class Solution(object):
         time = customers[0][0]
         answer = 0.0
 
-        for a, b in customers : 
-            if time > a : 
-                answer += (time + b - a)
+        for x in customers : 
+            if time > x[0] : 
+                answer += (time + x[1] - x[0])
             else : 
-                time = a
-                answer += b
+                time = x[0]
+                answer += x[1]
 
-            time += b 
+            time += x[1]
 
         return answer / len(customers)
         
