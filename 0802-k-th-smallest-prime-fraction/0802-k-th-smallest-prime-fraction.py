304f139a6f50d1c2e138a1ed5099e8ba72130cf4
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-09 16:20:53 +0900

    Time: 1067 ms (68.85%), Space: 27.4 MB (70.65%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
@@ -7,7 +7,6 @@ class Solution(object):
         """
         heap = []
         n = len(arr)
-        arr.sort()
 
         for i in range(n-1):
             res = float(arr[i])/float(arr[-1])
@@ -17,15 +16,8 @@ class Solution(object):
             res,l,r = heappop(heap)
             res = float(arr[l])/float(arr[r-1])
             heappush(heap,(res,l,r-1))
+
         res,l,r = heappop(heap)
-
-
-# 0, 3
-# 1, 3
-# 2, 3
-# 0, 2
-# 0, 1
-
 
         return [arr[l],arr[r]]
         
