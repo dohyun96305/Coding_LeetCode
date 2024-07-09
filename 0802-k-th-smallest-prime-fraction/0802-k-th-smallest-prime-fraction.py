4f7cdc6afa14a3360aac09733da58b77ae85529c
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-09 16:33:21 +0900

    Time: 1079 ms (67.91%), Space: 27.3 MB (70.65%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
@@ -1,3 +1,11 @@
+# K-th Smallest Prime Fraction
+# https://leetcode.com/problems/k-th-smallest-prime-fraction/description
+# Given a sorted array arr of unique integers containing 1 and prime numbers, return the k-th smallest fraction arr[i] / arr[j] (0 <= i < j < arr.length) as an array [arr[i], arr[j]].
+
+# heap
+
+from heapq import heappush, heappop
+
 class Solution(object):
     def kthSmallestPrimeFraction(self, arr, k):
         """
@@ -9,13 +17,13 @@ class Solution(object):
         n = len(arr)
 
         for i in range(n-1):
-            heappush(heap,(float(arr[i])/float(arr[-1]),i,n-1))
+            heappush(heap, (float(arr[i]) / float(arr[-1]), i, n-1))
         
         for i in range(k-1):
-            res,l,r = heappop(heap)
-            heappush(heap,(float(arr[l])/float(arr[r-1]),l,r-1))
+            _, l, r = heappop(heap)
+            heappush(heap, (float(arr[l]) / float(arr[r-1]), l, r-1))
 
-        res,l,r = heappop(heap)
+        _,l,r = heappop(heap)
 
         return [arr[l],arr[r]]
         
