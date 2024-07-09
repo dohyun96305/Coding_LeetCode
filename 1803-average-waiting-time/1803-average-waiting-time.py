abe653635be3c43770b07829fd417ba74602405e
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-09 14:39:39 +0900

    Time: 862 ms (7.5%), Space: 56.3 MB (12.5%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def averageWaitingTime(self, customers):
+        """
+        :type customers: List[List[int]]
+        :rtype: float
+        """
+        time = customers[0][0]
+        temp = 0.0
+
+        for a, b in customers : 
+            if time > a : 
+                temp += (time + b - a)
+            else : 
+                time = a
+                temp += b    
+            time += b 
+            print(temp)
+
+        return temp / len(customers)
+        
