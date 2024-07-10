441e2206b2ab94db69fa3202a4253ab20b6aa046
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-10 17:33:28 +0900

    Time: 356 ms (55.53%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-select
+# swap the seat id 
+# odd => last student not swapped
 
-case 
-when mod(id, 2) = 1 and id + 1 not in (select id from Seat) then id
-when mod(id, 2) = 1 then id + 1
-else id - 1
-end as id, student 
-
-from Seat order by id;
+SELECT CASE
+    WHEN MOD(ID, 2) = 1 AND ID + 1 NOT IN (SELECT ID FROM SEAT) THEN ID
+    WHEN MOD(ID, 2) = 1 THEN ID + 1
+    ELSE ID - 1
+END AS ID, STUDENT
+FROM SEAT
+ORDER BY ID;
