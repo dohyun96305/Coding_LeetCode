4d57a9a5913ebe8f62a0bd80cfc128cf96ac6564
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-07-16 17:06:46 +0900

    Time: 88 ms (41.48%), Space: 13.4 MB (25.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,6 +4,27 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if len(height) < 3:
+            return 0
+
+        trap_water = 0
+        left, right = 0, len(height) - 1
+        l_max, r_max = height[left], height[right]
+
+        while (left < right):
+            l_max, r_max = max(l_max, height[left]), max(r_max, height[right])
+
+            if l_max <= r_max:
+                trap_water += l_max - height[left]
+                left += 1
+
+            else:
+                trap_water += r_max - height[right]
+                right -= 1
+
+        return trap_water
+
+        '''
         n = len(height)
         if n == 0:
             return 0
@@ -25,3 +46,4 @@ class Solution(object):
             trappedWater += min(left[i], right[i]) - height[i]
 
         return trappedWater          
+        '''
