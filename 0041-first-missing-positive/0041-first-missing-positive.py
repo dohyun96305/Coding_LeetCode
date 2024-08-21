dfdc3aa74043a1a64da768964421e6f0508ceeb9
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-08-21 16:31:20 +0900

    Time: 411 ms (29.44%), Space: 22.3 MB (87.06%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,23 +1,19 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        # Cycle Sort
-        def swap(i,j):
-            nums[i],nums[j] = nums[j],nums[i]
-        i = 0
-
-        while(i<len(nums)):
-            if(0<nums[i]<=len(nums) and nums[i] != nums[nums[i]-1] and nums[i] != i):
-                swap(i,nums[i]-1)
-        
-            else:
-                i+=1
-        count = 1
         for i in range(len(nums)):
-            if(count == nums[i]):
-                count+=1
-        return count
+            if nums[i] <= 0:
+                nums[i] = len(nums) + 1
+        
+        # Marking visited indices
+        for i in range(len(nums)):
+            idx = abs(nums[i]) - 1
+            if 0 <= idx < len(nums):
+                if nums[idx] > 0:
+                    nums[idx] = -nums[idx]
+        
+        # Finding the smallest missing positive integer
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return i + 1
+        
+        return len(nums) + 1
