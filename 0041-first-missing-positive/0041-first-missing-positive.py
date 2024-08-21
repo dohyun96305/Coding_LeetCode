b5addcccc448c58b4831012c692e8409e5380053
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-08-21 16:34:11 +0900

    Time: 358 ms (87.98%), Space: 28.8 MB (8.9%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,19 +1,24 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        for i in range(len(nums)):
-            if nums[i] <= 0:
-                nums[i] = len(nums) + 1
-        
-        # Marking visited indices
-        for i in range(len(nums)):
-            idx = abs(nums[i]) - 1
-            if 0 <= idx < len(nums):
-                if nums[idx] > 0:
-                    nums[idx] = -nums[idx]
-        
-        # Finding the smallest missing positive integer
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                return i + 1
-        
-        return len(nums) + 1
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        temp = 0
+
+        nums = list(set(nums))
+        nums.sort()
+
+        for i in nums : 
+            if i <= 0 : 
+                pass 
+
+            else : 
+                if i == temp + 1 : 
+                    temp = i
+                else : 
+                    break
+
+        return temp + 1
+                
+      
