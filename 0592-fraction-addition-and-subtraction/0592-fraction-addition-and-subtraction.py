a7c9ef53555cf65ef6bd69378fd7c60fb446ff49
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-08-23 17:04:55 +0900

    Time: 24 ms (8.33%), Space: 11.7 MB (50%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -9,32 +9,36 @@ class Solution(object):
                 a, b = b, a % b
             return abs(a)
         
-        def add1(a, b, symbols) : 
-            a_0, a_1 = a.split('/')
-            b_0, b_1 = b.split('/')
+        def add1(a_0, a_1, b_0, b_1, symbol):
+            c_0 = a_0 * b_1 + symbol * a_1 * b_0
+            c_1 = a_1 * b_1
 
-            c_0 = str(int(a_0) * int(b_1) + symbols * (int(a_1) * int(b_0)))
-            c_1 = str(int(a_1) * int(b_1))
+            return c_0, c_1
 
-            return c_0 + '/' + c_1
+        symbols = list(map(lambda x: -1 if x == '-' else 1, re.findall(r'[-+]', expression)))
+        nums = re.split(r"[-+]", expression)
 
-        symbols = list(map(lambda x: -1 if x == '-' else 1, re.findall(r'[-+]', expression)))[::-1]
-        nums = list(re.split(r"[-+]", expression))[::-1]
+        print(nums, symbols)
+        
+        if not nums[0] : 
+            nums = nums[1:]
 
-        if not nums[-1] : 
-            nums.pop()
-            a_0, a_1 = nums.pop().split('/')
-            a = str(symbols.pop() * int(a_0)) + '/' + a_1
+            a_0, a_1 = map(int, nums[0].split('/'))
+            a_0 *= symbols[0]
+
+            symbols = symbols[1:]
 
         else : 
-            a = nums.pop()
-        
-        while symbols : 
-            b = nums.pop()
-            a = add1(a, b, symbols.pop())
-        
-        a_0, a_1 = a.split('/')
-        
-        temp = gcd(int(a_0), int(a_1))
+            a_0, a_1 = map(int, nums[0].split('/'))
+
+
+
+        for i in range(1, len(nums)) :
+            b_0, b_1 = map(int, nums[i].split('/'))
+            symbol = symbols[i-1]
+
+            a_0, a_1 = add1(a_0, a_1, b_0, b_1, symbol)
+
+        temp = gcd(a_0, a_1)
 
         return str(int(a_0) // temp) + '/' + str(int(a_1) // temp)
