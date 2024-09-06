32b277f478625024b6da90b10c807f729a69ef9f
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-09-06 16:41:17 +0900

    Time: 790 ms (43.43%), Space: 0B (100%) - LeetHub

---
+++ 2315-the-change-in-global-rankings/2315-the-change-in-global-rankings.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+# National team, sorting points desc
+# same points => sorting name lexicographical order
+
+WITH TEMP AS (
+    SELECT A.TEAM_ID, A.NAME, A.POINTS,
+        ROW_NUMBER() OVER (ORDER BY A.POINTS DESC, A.NAME) AS PREV_RANK, 
+        ROW_NUMBER() OVER (ORDER BY A.POINTS + B.POINTS_CHANGE DESC, A.NAME) AS CURR_RANK
+    FROM TEAMPOINTS AS A
+    JOIN POINTSCHANGE AS B ON A.TEAM_ID = B.TEAM_ID
+    )
+
+SELECT TEAM_ID, NAME, 
+    CASE 
+        WHEN PREV_RANK >= CURR_RANK THEN CAST(PREV_RANK - CURR_RANK AS SIGNED)
+        ELSE CAST(CONCAT('-', CURR_RANK - PREV_RANK) AS SIGNED)
+    END AS RANK_DIFF
+FROM TEMP 
+
+
+
