281d08bf6988793b491b824f994c9f6845f768d2
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2024-09-23 17:07:24 +0900

    Time: 1765 ms (5.56%), Space: 22.7 MB (37.96%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
@@ -1,4 +1,18 @@
 class Solution(object):
+    def check(self, position, m, num1) : 
+        prev = position[0]
+        count = 1
+
+        for i in range(1, len(position)) : 
+            if position[i] - prev >= num1 : 
+                prev = position[i]
+                count += 1
+            
+            if count == m : 
+                return True
+            
+        return False
+
     def maxDistance(self, position, m):
         """
         :type position: List[int]
@@ -8,26 +22,12 @@ class Solution(object):
         position.sort()
         answer = 0
 
-        def check(num1) : 
-            prev = position[0]
-            count = 1
-
-            for i in range(1, len(position)) : 
-                if position[i] - prev >= num1 : 
-                    prev = position[i]
-                    count += 1
-                
-                if count == m : 
-                    return True
-                
-            return False
-
-        left, right = 0, (position[-1] - position[0])
+        left, right = 1, (position[-1] - position[0])
 
         while left <= right : 
             mid = (left + right) // 2
 
-            if check(mid) : 
+            if self.check(position, m, mid) : 
                 answer = mid
                 left = mid + 1
             else : 
