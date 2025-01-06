ec6869aff714f7f564052232f28fa013b8bfe62b
Dohyun Yoon <75681704+dohyun96305@users.noreply.github.com>
2025-01-06 17:36:18 +0900

    Time: 38 ms (52.79%), Space: 17.8 MB (17.77%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,13 +1,5 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         temp = (32 - len(bin(n)[2:])) * '0' + bin(n)[2:]
-        count = 0
-        answer = 0 
-
-        for i in temp : 
-            if i == '1' : 
-                answer += (2 ** count)
-
-            count += 1
-
-        return answer
+ 
+        return int(temp[::-1], 2)
